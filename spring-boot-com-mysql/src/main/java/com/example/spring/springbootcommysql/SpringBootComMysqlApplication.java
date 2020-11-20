f081b9d141498a953dd58ea691400bee9744fdb8
Lucas Gregorio Martins <lucas.martins@tqi.com.br>
2020-11-20 14:45:42 -0300

    Adicionado arquivos Docker e configuração do DataSource do banco

---
+++ spring-boot-com-mysql/src/main/java/com/example/spring/springbootcommysql/SpringBootComMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.springbootcommysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootComMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootComMysqlApplication.class, args);
+	}
+
+}
---
+++ spring-boot-com-mysql/src/test/java/com/example/spring/springbootcommysql/SpringBootComMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.springbootcommysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootComMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
